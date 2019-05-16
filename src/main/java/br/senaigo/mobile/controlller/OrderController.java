05dcbe645c2c77eab7f1b68cba70941da4454003
Bruno Rodrigues <bruno@urbano.eti.br>
2019-05-16 19:28:56 -0300

    * Insert dependency to sl4j to write in console application

--- src/main/java/br/senaigo/mobile/controlller/OrderController.java
+++ src/main/java/br/senaigo/mobile/controlller/OrderController.java
@@ -2,6 +2,8 @@ package br.senaigo.mobile.controlller;
 
 import static org.springframework.hateoas.mvc.ControllerLinkBuilder.linkTo;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.hateoas.Link;
 import org.springframework.hateoas.MediaTypes;
@@ -12,17 +14,28 @@ import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.senaigo.mobile.entities.Order;
 import br.senaigo.mobile.interfaces.GenericOperationsController;
 import br.senaigo.mobile.service.OrderService;
-
+/**
+ * 
+ * @author bruno
+ *
+ */
 @RestController("/orders")
 public class OrderController implements GenericOperationsController<Order> {
+	
+	/**
+	 * @see http://appsdeveloperblog.com/spring-boot-logging-with-loggerfactory/
+	 */
+	Logger logger = LoggerFactory.getLogger(OrderController.class);
 	
 	@Autowired
 	public OrderService orderService;
@@ -33,8 +46,7 @@ public class OrderController implements GenericOperationsController<Order> {
 						 	 MediaType.APPLICATION_XML_VALUE,
 						 	 MediaTypes.HAL_JSON_VALUE})
 	@ResponseStatus(HttpStatus.CREATED)
-	public Resource<Order> post(Order order) {
-		
+	public Resource<Order> post(@RequestBody Order order) {
 		orderService.post(order);
 		
 		Link link = linkTo(OrderController.class).slash(order.getId()).withSelfRel();
@@ -45,7 +57,7 @@ public class OrderController implements GenericOperationsController<Order> {
 	@Override
 	@PutMapping(consumes = { MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE })
 	@ResponseStatus(HttpStatus.NO_CONTENT)
-	public void put(Order entity) {
+	public void put(@RequestBody Order entity) {
 		// TODO Auto-generated method stub
 
 	}
@@ -53,7 +65,7 @@ public class OrderController implements GenericOperationsController<Order> {
 	@Override
 	@DeleteMapping(consumes = { MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE })
 	@ResponseStatus(HttpStatus.NO_CONTENT)
-	public void delete(Order entity) {
+	public void delete(@RequestBody Order entity) {
 		// TODO Auto-generated method stub
 
 	}
@@ -69,12 +81,13 @@ public class OrderController implements GenericOperationsController<Order> {
 	}
 
 	@Override
-	@GetMapping(consumes = { MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE }, 
+	@GetMapping(value="/{id}",
+				consumes = { MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE }, 
 	 			produces = {MediaType.APPLICATION_JSON_VALUE,
 	 						MediaType.APPLICATION_XML_VALUE,
 	 						MediaTypes.HAL_JSON_VALUE})
 	@ResponseStatus(HttpStatus.OK)
-	public Resource<Order> get(Integer id) {
+	public Resource<Order> get(@PathVariable("id") Integer id) {
 		// TODO Auto-generated method stub
 		return null;
 	}
@@ -82,7 +95,7 @@ public class OrderController implements GenericOperationsController<Order> {
 	@Override
 	@PatchMapping(consumes = { MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE })
 	@ResponseStatus(HttpStatus.NO_CONTENT)
-	public void patch(Order entity) {
+	public void patch(@RequestBody Order entity) {
 		// TODO Auto-generated method stub
 
 	}
