3430bceb83263be84607724242d6c308949b94f3
Bruno Rodrigues <bruno@urbano.eti.br>
2019-05-16 19:55:35 -0300

    * Insert logger.info * Try-catch block

--- src/main/java/br/senaigo/mobile/controlller/OrderController.java
+++ src/main/java/br/senaigo/mobile/controlller/OrderController.java
@@ -2,6 +2,8 @@ package br.senaigo.mobile.controlller;
 
 import static org.springframework.hateoas.mvc.ControllerLinkBuilder.linkTo;
 
+import java.util.List;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -47,26 +49,42 @@ public class OrderController implements GenericOperationsController<Order> {
 						 	 MediaTypes.HAL_JSON_VALUE})
 	@ResponseStatus(HttpStatus.CREATED)
 	public Resource<Order> post(@RequestBody Order order) {
-		orderService.post(order);
-		
-		Link link = linkTo(OrderController.class).slash(order.getId()).withSelfRel();
-		Resource<Order> result = new Resource<Order>(order,link);
-		return result;
+		try {
+			
+			orderService.post(order);
+			logger.info(String.format("Registro inserido: %s",order.toString()));
+			
+			Link link = linkTo(OrderController.class).slash(order.getId()).withSelfRel();
+			Resource<Order> result = new Resource<Order>(order,link);
+			return result;
+		} catch (Exception e) {
+			logger.error(String.format("Erro ao executar o método POST.\nMensagem: %s",e.getMessage()));
+		}
+		return null;
 	}
 
 	@Override
 	@PutMapping(consumes = { MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE })
 	@ResponseStatus(HttpStatus.NO_CONTENT)
-	public void put(@RequestBody Order entity) {
-		// TODO Auto-generated method stub
-
+	public void put(@RequestBody Order order) {
+		try {
+			orderService.put(order);
+			logger.info(String.format("Registro atualizado: %s",order.toString()));
+		} catch (Exception e) {
+			logger.error(String.format("Erro ao executar o método PUT.\nMensagem: %s",e.getMessage()));
+		}
 	}
 
 	@Override
 	@DeleteMapping(consumes = { MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE })
 	@ResponseStatus(HttpStatus.NO_CONTENT)
-	public void delete(@RequestBody Order entity) {
-		// TODO Auto-generated method stub
+	public void delete(@RequestBody Order orders) {
+		try {
+			orderService.delete(orders);
+			logger.info(String.format("Registro(s) deletado(s): %s",orders.toString()));
+		} catch (Exception e) {
+			logger.error(String.format("Erro ao executar o método PUT.\nMensagem: %s",e.getMessage()));
+		}
 
 	}
 
@@ -76,7 +94,25 @@ public class OrderController implements GenericOperationsController<Order> {
 							 MediaTypes.HAL_JSON_VALUE})
 	@ResponseStatus(HttpStatus.OK)
 	public Resources<Order> get() {
-		// TODO Auto-generated method stub
+		try {
+			List<Order> orders = orderService.get();
+			
+			logger.info(String.format("Registro(s) recuperados(s): %s",orders.toString()));
+			
+//			for (final Order order : orders) {
+//		        Link selfLink = linkTo(methodOn(OrderController.class)
+//		          .getOrderById(customerId, order.getIdOrder()())).withSelfRel();
+//		        order.add(selfLink);
+//		    }
+//		  
+//		    Link link = linkTo(methodOn(CustomerController.class)
+//		      .getOrdersForCustomer(customerId)).withSelfRel();
+//		    Resources<Order> result = new Resources<Order>(orders, link);
+//		    return result;
+			
+		} catch (Exception e) {
+			logger.error(String.format("Erro ao executar o método GET.\nMensagem: %s",e.getMessage()));
+		}
 		return null;
 	}
 
@@ -88,15 +124,25 @@ public class OrderController implements GenericOperationsController<Order> {
 	 						MediaTypes.HAL_JSON_VALUE})
 	@ResponseStatus(HttpStatus.OK)
 	public Resource<Order> get(@PathVariable("id") Integer id) {
-		// TODO Auto-generated method stub
+		try {
+			Order order = orderService.get(Order.builder().idOrder(id).build());
+			logger.info(String.format("Registro recuperado: %s",order.toString()));
+		} catch (Exception e) {
+			logger.error(String.format("Erro ao executar o método GET.\nMensagem: %s",e.getMessage()));
+		}
 		return null;
 	}
 
 	@Override
 	@PatchMapping(consumes = { MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE })
 	@ResponseStatus(HttpStatus.NO_CONTENT)
-	public void patch(@RequestBody Order entity) {
-		// TODO Auto-generated method stub
+	public void patch(@RequestBody Order order) {
+		try {
+			orderService.patch(order);
+			logger.info(String.format("Registro atualizado: %s",order.toString()));
+		} catch (Exception e) {
+			logger.error(String.format("Erro ao executar o método PATCH.\nMensagem: %s",e.getMessage()));
+		}
 
 	}
 
